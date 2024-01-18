af612b9770d8f680bf643a31adbee8de6be95b77
WeleSS2 <86500144+WeleSS2@users.noreply.github.com>
2024-01-18 19:26:24 +0100

    SDL

--- main.cpp
+++ main.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
 #include <stdio.h>
+#include "SDL.h"
 
-int main()
+#undef main
+
+int main(int argc, char **argv)
 {
+    SDL_Rect aaa;
+    
     std::cout << "Hello team!" << std::endl;
     printf("Do not limit youself, seek for all possible solutions! \n");
+    
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // Create a renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // Set the draw color
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+    // Clear the screen
+    SDL_RenderClear(renderer);
+
+    // Present the renderer
+    SDL_RenderPresent(renderer);
+
+    // Wait for a few seconds
+    SDL_Delay(2000);
+
+    // Destroy window and renderer
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    // Quit SDL
+    SDL_Quit();
+
     return 0;
 }
